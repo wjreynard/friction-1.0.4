8c51275bce7b5a8deca0c5199db27d4dcb42da97
Will Reynard <44241583+wjreynard@users.noreply.github.com>
2020-07-06 15:48:20 +0100

    Updated levels, updated main menu, fixed sensitivity issue, added credits screen

--- Assets/0_scripts/GameManager.cs
+++ Assets/0_scripts/GameManager.cs
@@ -6,9 +6,9 @@ public class GameManager : MonoBehaviour {
     
 	void Start () {
         // On game start, initialise...
-        //PlayerPrefs.GetFloat("playerSens", 1); // Set to default of one
         PlayerPrefs.SetFloat("Collectibles", 0.1f);
-        
+        PlayerPrefs.SetFloat("playerSens", 1.0f);
+
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
     }
